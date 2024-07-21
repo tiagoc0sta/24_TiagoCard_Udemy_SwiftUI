9ec77c0a1bafbe98717520374b726c663492d013
tiagoc0sta <tiagodpv@gmail.com>
2024-07-21 12:40:37 -0400

    Initial Commit

---
+++ TiagoCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TiagoCard
+//
+//  Created by Tiago de Freitas Costa on 2024-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TiagoCard/TiagoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiagoCardApp.swift
+//  TiagoCard
+//
+//  Created by Tiago de Freitas Costa on 2024-07-21.
+//
+
+import SwiftUI
+
+@main
+struct TiagoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
