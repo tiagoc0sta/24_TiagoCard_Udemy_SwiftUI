8909f845282b10e24bc9025c40f279c35f804dd4
tiagoc0sta <tiagodpv@gmail.com>
2024-07-21 21:33:12 -0400

    f

--- TiagoCard/ContentView.swift
+++ TiagoCard/ContentView.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("tiago")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5))
+                Text("Tiago Costa")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                InfoView(text: "44 123 456 789", imageName: "phone.fill")
+                InfoView(text: "tiagodpv@gmail.com", imageName: "envelope.fill")
+            }
         }
-        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
--- TiagoCard/InfoView.swift
+++ TiagoCard/InfoView.swift
@@ -5,4 +5,29 @@
 //  Created by Tiago de Freitas Costa on 2024-07-21.
 //
 
-import Foundation
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50.0)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text)
+                })
+            .padding(.all)
+    }
+}
+
+
+#Preview {
+    InfoView(text: "Hello", imageName: "phone.fill")
+        .previewLayout(.sizeThatFits)
+}
